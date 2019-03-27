1278cc7ddaeca5cf60626a0276643b9fa7343021
SCaryon <1241466849@qq.com>
2019-03-27 21:11:06 +0800

    修复重大BUG

--- mysite/mysite/Allocation.py
+++ mysite/mysite/Allocation.py
@@ -1,5 +1,5 @@
 from queue import PriorityQueue as PQueue
-import json,os
+import json,os,copy
 import numpy as np
 class Node(object):
     def __init__(self,NeedTime,Custom):
@@ -22,7 +22,7 @@ class AllocatCls(object):
     MaxSource = []                                          # 用户一共所需要的资源量
     CustomInit = []                                         # 用户初始资源量
     SystemInit = []                                         # 系统初始有的资源量
-    WaitQueue  = PQueue()                                   # 等待队列，数据结构为优先队列
+    # WaitQueue  = PQueue()                                   # 等待队列，数据结构为优先队列
     
     # 类的初始化，需要输入用户数量和资源数量
     def __init__(self,NumCustom,NumResource):              
@@ -37,94 +37,141 @@ class AllocatCls(object):
         self.NeedTime = NeedTime
         
     # 把得到的安全序列输入到指定文件
-    def WriteFile(self):
+    def WriteFile(self,WaitQueue):
+        # print('************safe***************')
+        # print(self.Answer,'\n\n\n\n')
         TmpTime = 0     # 保存这组安全序列需要的时间
         TmpQueue = []   # 保存序列，用于恢复
         # 遍历优先队列，得到总时间
-        while self.WaitQueue.empty() == False:
-            Tmp = self.WaitQueue.get()
+        while WaitQueue.empty() == False:
+            Tmp = WaitQueue.get()
             TmpQueue.append(Tmp)
-            if self.WaitQueue.qsize() == 0:
+            if WaitQueue.qsize() == 0:
                 TmpTime = Tmp.NeedTime
 
         # 优化剪枝条件
         self.TimeMinl = min(self.TimeMinl,TmpTime)
         for it in TmpQueue:
-            self.WaitQueue.put(it)
+            WaitQueue.put(it)
         # 写入指定json文件
-        size = os.path.getsize('./'+self.OutFileName)
-        with open(self.OutFileName,'r') as file_object:
-            now = file_object.read()
-        with open(self.OutFileName,'w') as file_object:
-            if  size ==0:
+        if os.path.exists(self.OutFileName):
+            size = os.path.getsize('./'+self.OutFileName)
+            with open(self.OutFileName,'r') as file_object:
+                now = file_object.read()
+            with open(self.OutFileName,'w') as file_object:
+                if  size ==0:
+                    tmp=dict()
+                else:
+                    tmp = json.loads(now)
+                tmp[self.NumSafe]=(self.Answer,TmpTime)
+                json.dump(tmp,file_object,indent=4)
+        else:
+            with open(self.OutFileName,'w') as file_object:
                 tmp=dict()
-            else:
-                tmp = json.loads(now)
-            tmp[self.NumSafe]=(self.Answer,TmpTime)
-            json.dump(tmp,file_object,indent=4)
+                tmp[self.NumSafe]=(self.Answer,TmpTime)
+                json.dump(tmp,file_object,indent=4)
+        
     
     # 用于外部调用，开始查找安全序列
     def Run(self):
         self.Answer=[None]*self.NumCustom
-        self.Search(0,self.SystemInit,self.CustomInit,True,0)
+        self.Search(0,self.SystemInit,self.CustomInit,True,0,PQueue())
 
         
     # DFS的过程，每次需要当前的状态
-    def Search(self,TimeNow,System,AllocatedSource,CanAlloc,deep):
-        if self.NumSafe >= 100:
-            return
+    def Search(self,TimeNow,System,AllocatedSource,CanAlloc,deep,WaitQueue):
+        # print('Time=',TimeNow)
+        # print('System=',System)
+        # print('AllocatedSource=',AllocatedSource)
+        # print('CanAlloc=',CanAlloc)
+        # print('Deep=',deep)
+        # print("QueueSize=",WaitQueue.qsize())
+        # que=[]
+        # while WaitQueue.empty() == False:
+        #     tmp=WaitQueue.get()
+        #     print('Time=',tmp.NeedTime)
+        #     print('Custom=',tmp.Custom,'\n')
+        #     que.append(tmp)
+        # for i in que:
+        #     WaitQueue.put(i)
+        # print('Answer=',self.Answer,'\n\n')
+        # if self.NumSafe >= 100:
+        #     return
         if CanAlloc == False:                   # 无法分配，释放资源，时间跳到下一个节点
-            if self.WaitQueue.qsize() == 0:     # 死锁，直接返回
+            if WaitQueue.qsize() == 0:     # 死锁，直接返回
                 return 
             else:                               # 出队
-                tmp = self.WaitQueue.get()
+                tmp = WaitQueue.get()
                 TimeNow = TimeNow + tmp.NeedTime
-                for i in range(len(self.MaxSource[tmp.Custom])):
+                for i in range(self.NumResource):
                     System[i] = System[i] + self.MaxSource[tmp.Custom][i]
 
         if TimeNow > self.TimeMinl:            # 剪枝条件
             return
         if deep == self.NumCustom:              # 得到安全序列，输出到文件，回溯
             self.NumSafe = self.NumSafe + 1
-            self.WriteFile()
+            self.WriteFile(WaitQueue)
             return
 
         CustomNeed = [None] * self.NumCustom
-        for key in range(len(self.MaxSource)):  # 得到当前用户需要的资源数量
+        for key in range(self.NumCustom):  # 得到当前用户需要的资源数量
             CustomNeed[key] = [None]*self.NumResource
-            for i in range(len(self.MaxSource[key])):
+            for i in range(self.NumResource):
                 CustomNeed[key][i] = self.MaxSource[key][i] - AllocatedSource[key][i]
 
-        for key in range(len(self.MaxSource)):  # 判断是否能进行分配
-            if key in self.Answer:
+        for key in range(self.NumCustom):  # 遍历每个用户判断是否能进行分配
+            if key in self.Answer:# 判定重复
                 continue
             flag = False                        # flag为False为能分配
-            for i in range(len(CustomNeed[key])):
+            for i in range(self.NumResource):
                 if System[i] < CustomNeed[key][i]:
                     flag = True;break
             if flag == False:
                 CanAlloc = True
-                for i in range(len(CustomNeed[key])):       # 扣除该用户需要的资源
-                    System[i] = System[i] - CustomNeed[key][i]
-                    AllocatedSource[key][i] = self.MaxSource[key][i]
+                # 深拷贝一堆参数
+                TmpAllcatedSource = copy.deepcopy(AllocatedSource) 
+                TmpSystem = copy.deepcopy(System)
+                TmpQueue = PQueue()
+                TmpVector = []
+                while WaitQueue.empty() == False:
+                    Tmp = copy.deepcopy(WaitQueue.get())
+                    Tmp.append(Tmp)
+                    TmpQueue.put(Tmp) 
+                for it in TmpVector:
+                    WaitQueue.put(it)
+                # print('TmpSystem=',TmpSystem)
+                # print('TmpAllcatedSource=',TmpAllcatedSource)
+                # print('AllocatedSource=',AllocatedSource)
+                # print('Need=',CustomNeed)
+
+                for i in range(self.NumResource):       # 扣除该用户需要的资源
+                    TmpSystem[i] = System[i] - CustomNeed[key][i]
+                    TmpAllcatedSource[key][i] = self.MaxSource[key][i]
                 # 标记安全序列
                 self.Answer[deep] = key
-                self.WaitQueue.put(Node(self.NeedTime[key] + TimeNow,key))
-                self.Search(TimeNow,System,AllocatedSource,CanAlloc,deep+1)     
-                self.Answer[deep] = None
+                TmpQueue.put(Node(self.NeedTime[key] + TimeNow,key))
+                # print('Key=',key)
+                # print('AfterTmpSystem=',TmpSystem)
+                # print('AfterTmpAllcatedSource=',TmpAllcatedSource)
+                self.Search(TimeNow,TmpSystem,TmpAllcatedSource,CanAlloc,deep+1,TmpQueue)     
                 # 满足分配条件，进行分配
+                self.Answer[deep] = None
+
             else:
                 CanAlloc = False
                     
         if CanAlloc == False: # 不满足分配条件，进入递归
-            self.Search(TimeNow,System,AllocatedSource,CanAlloc,deep)
+            self.Search(TimeNow,System,AllocatedSource,CanAlloc,deep,WaitQueue)
 
 
 if __name__ == "__main__":
-    NumCustom=10
-    NumSource=10
-    DataMaker = RandomData()
-    Available, MaxSource, AllocatedSource, NeedTime=DataMaker.random_Data(NumCustom,NumSource,10000)
-    test = AllocatCls(NumCustom,NumSource)
+    with open('data.json','r') as file_object:
+            now = file_object.read()
+    data = json.loads(now)
+    Available = data['Available']
+    MaxSource = data['MaxSource']
+    AllocatedSource = data['AllocatedSource']
+    NeedTime = data['NeedTime']
+    test = AllocatCls(3,3)
     test.LoadData(Available, MaxSource, AllocatedSource, NeedTime)
     test.Run()
